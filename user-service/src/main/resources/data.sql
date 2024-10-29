45714824352bd3af3bc9e03ac91ea19e2e182886
its-sky <smc9919@naver.com>
2024-10-29 13:32:35 +0900

    feat: Catalog 마이크로서비스 Mock 데이터 추가

---
+++ user-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO CATALOG(product_id, product_name, stock, unit_price)
+        VALUES ('CATALOG-001', 'Berlin', 100, 1500),
+               ('CATALOG-002', 'Seoul', 250, 1000),
+               ('CATALOG-003', 'London', 340, 1230);
